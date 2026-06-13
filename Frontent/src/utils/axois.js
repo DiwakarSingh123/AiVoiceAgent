64f753ab40fdb1516e7cf4c66d0399ffe1d59e94
Diwakar Singh <diwakarsinghdd111@gmail.com>
2026-06-13 12:55:21 +0530

    chore: update axios client to use railway backend

--- Frontent/src/utils/axois.js
+++ Frontent/src/utils/axois.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: 'https://aivoiceagent-backend.onrender.com',
+    baseURL: 'https://aivoiceagent-production-1a3f.up.railway.app',
     withCredentials:true,
     headers: {
         'Content-Type': 'application/json'
